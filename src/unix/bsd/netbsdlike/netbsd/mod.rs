94d75760424f8e3aa408f3ea6f40e4ae3a9cdcff
Jonas Lindmark <jonas@denacode.se>
2025-09-05 14:08:26 +0200

    Add m68k support
    (cherry picked from commit 35f4924f7a4cbd56b8b2de781c97e0802ce84e00)

--- src/unix/bsd/netbsdlike/netbsd/mod.rs
+++ src/unix/bsd/netbsdlike/netbsd/mod.rs
@@ -2996,6 +2996,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "riscv64")] {
         mod riscv64;
         pub use self::riscv64::*;
+    } else if #[cfg(target_arch = "m68k")] {
+        mod m68k;
+        pub use self::m68k::*;
     } else {
         // Unknown target_arch
     }
